4d9d5be4f6967b57c0e22cb775869637a6cfa8ff
grzegorz kabzinski <grzesiek.kab@gmail.com>
2019-04-08 17:47:06 +0200

    isolation issuance with X positions should call calculateTax X amount of times
    :wq
    :wq

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -74,5 +74,21 @@ public class BookKeeperTest {
         bookKeeper.issuance(invoiceRequest, taxPolicy);
         Mockito.verify(invoiceFactorySpy, Mockito.times(1)).create(Matchers.any(ClientData.class));
     }
+
+    @Test
+    public void testIssuanceWithThreePositionsShouldCallCalculateTaxThreeTimes(){
+        ProductData productData = Mockito.mock(ProductData.class);
+        TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
+        InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(new Id("1"),"Bob"));
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+        Mockito.when(taxPolicy.calculateTax(Matchers.any(ProductType.class), Matchers.any(Money.class))).thenReturn(new Tax(new Money(1.99), "Tax Description"));
+        invoiceRequest.add(new RequestItem(productData,1, new Money(1.99)));
+        invoiceRequest.add(new RequestItem(productData,1, new Money(1.99)));
+        invoiceRequest.add(new RequestItem(productData,1, new Money(1.99)));
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+        Mockito.verify(taxPolicy, Mockito.times(3)).calculateTax(Matchers.any(ProductType.class),Matchers.any(Money.class));
+    }
+
     
+
 }
